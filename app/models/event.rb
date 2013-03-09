97f5ca2e703da80f82fe1b1bed3f6a3ecab85302
Lionel E. Ramos <lionelramos@hotmail.com>
2013-03-09 15:49:29 -0700

    ready to push to Heroku

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,6 @@
 class Event < ActiveRecord::Base
+
+	attr_accessible :title, :date, :description
+	validates_presence_of :title, :date, :description
+	validates_uniqueness_of :title
 end
--- config/application.rb
+++ config/application.rb
@@ -52,6 +52,7 @@ module AAAWebsite
 
     # Enable the asset pipeline
     config.assets.enabled = true
+    config.active_record.timestamped_migrations = true
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
